20384b1fa71caedda8173453ab05d1ac73783ce8
1rfxn <legendarybasha786@gmail.com>
2026-04-15 18:13:05 +0530

    Time: 5 ms (41.07%) | Memory: 42.3 MB (96.07%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int res = 1;
+        
+        while(n >= res) {
+            n -= res;
+            res++;
+        }
+        
+        return res - 1;
+    }
+}
