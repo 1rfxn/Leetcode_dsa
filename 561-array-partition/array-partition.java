b7ecf19cfd5fa3f24c7f11b5248d7c62dfe7c03a
1rfxn <irfanbashahere@gmail.com>
2026-05-08 16:48:52 +0530

    Time: 17 ms (80.65%) | Memory: 49.6 MB (52.08%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int res = 0;
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length - 1 ; i += 2)
+        {
+            res += nums[i];
+        }
+        return res;
+    }
+}
