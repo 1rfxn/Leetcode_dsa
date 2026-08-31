6116b6d54c7bc69a1446fe2b301a8a186a348ed3
1rfxn <irfanbashahere@gmail.com>
2026-08-31 06:41:30 +0530

    Medium - 445. Add Two Numbers II from LeetCode

--- LeetCode/Java/Medium/445. Add Two Numbers II/Solution.java
+++ LeetCode/Java/Medium/445. Add Two Numbers II/Solution.java
@@ -10,29 +10,56 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Long n1 = 0l;
-        Long n2 = 0l;
-        ListNode d1 = l1;
-        ListNode d2 = l2;
-        while(d1 != null)
-        {
-            n1 = n1 * 10 + d1.val;
-            d1 = d1.next;
-        }
-        while(d2 != null)
-        {
-            n2 = n2 * 10 + d2.val;
-            d2 = d2.next;
-        }
-        Long n = n1 + n2;
-        String num = n + "";
+
+        l1 = rev(l1);
+        l2 = rev(l2);
+
         ListNode res = new ListNode();
         ListNode dmy = res;
-        for(int i = 0 ; i < num.length() ; i++)
-        {
-            dmy.next = new ListNode(num.charAt(i) - '0');
+
+        int c = 0;
+
+        while (l1 != null || l2 != null) {
+
+            int num = c;
+
+            if (l1 != null) {
+                num += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                num += l2.val;
+                l2 = l2.next;
+            }
+
+            c = num / 10;
+            num = num % 10;
+
+            dmy.next = new ListNode(num);
             dmy = dmy.next;
         }
-        return res.next;
+
+        // If carry is still remaining
+        if (c != 0) {
+            dmy.next = new ListNode(c);
+        }
+
+        return rev(res.next);
+    }
+
+    private ListNode rev(ListNode l) {
+
+        ListNode r = null;
+        ListNode d = l;
+
+        while (d != null) {
+            ListNode t = d.next;
+            d.next = r;
+            r = d;
+            d = t;
+        }
+
+        return r;
     }
 }
