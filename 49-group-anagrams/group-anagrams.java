52b676f74710212fd2ea018cd04762ea647c27f4
1rfxn <irfanbashahere@gmail.com>
2026-09-12 20:09:59 +0530

    Time: 7 ms (71.83%) | Memory: 49.5 MB (72.52%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> m = new HashMap<>();
+        for(int i = 0 ; i < strs.length ; i++)
+        {
+            char[] c = strs[i].toCharArray();
+            Arrays.sort(c);
+            String k = new String(c);
+            if(!m.containsKey(k))
+                m.put(k,new ArrayList<>());
+            m.get(k).add(strs[i]);
+        }
+        return new ArrayList<>(m.values());
+    }
+}
